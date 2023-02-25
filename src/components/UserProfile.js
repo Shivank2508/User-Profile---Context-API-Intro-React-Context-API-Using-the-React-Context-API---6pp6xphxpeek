0a5b1add8061eedc4ddc568d3d29d7d490b38c4e
ShivankTyagi <tyagishivank1212@gmail.com>
2023-02-25 22:13:57 +0530

    t

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,8 +2,15 @@ import { useContext } from 'react'
 import React from 'react'
 import { UserContext } from "./App"
 
-const UserProfile = () =>{
+const UserProfile = () => {
+
+    let localContext = useContext(UserContext);
+
+    return (
+        <>
+            <h1 id="name">Name:- {localContext.name}</h1>
+            <h1 id="age">Age:- {localContext.age}</h1>
+        </>
+    )
 
 }
-
-export {UserProfile}
